f45367795431ae798f741f08a778af91fd200ac0
KevinYang <chgc.tw@gmail.com>
2017-04-30 14:54:29 +0800

    完成基本數字按鍵顯示功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  displayValue: string = '0';
+  constructor() {}
+  ngOnInit() {}
+
+  inputDigit(digit: number) {
+    this.displayValue = this.displayValue === '0' ?
+        String(digit) :
+        this.displayValue + String(digit);
+  }
+
+  inputDot() {
+    if (this.displayValue.indexOf('.') === -1) {
+      this.displayValue += '.';
+    }
+  }
+
+  clearDisplay() {
+    this.displayValue = '0';
+  }
+
+  toggleSign() {
+    this.displayValue = this.displayValue.charAt(0) === '-' ?
+        this.displayValue.substr(1) :
+        '-' + this.displayValue;
+  }
+
+  inputPercent() {
+    this.displayValue = String(parseFloat(this.displayValue) / 100);
+  }
 }
